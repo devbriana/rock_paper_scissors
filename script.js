c0122fad3f656d357e1a4304f123083a8156a6d0
devbriana <dev.brianam@gmail.com>
2025-04-11 15:59:59 -0400

    Style game with JavaScript DOM
    - Replaced console.logs with DOM methods for user feedback.
    - Styled the page using JavaScript for additional DOM practice.
    - Game functions correctly; JS organization may need review.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// since last commit: 
+// changed console.logs into DOM methods - game functions correctly
+// started styling page through javaScript
+
+
+
+
+
 let computerChoice;
 let humanChoice;
 
@@ -8,39 +16,64 @@ var currentRound = 1;
 var roundLimit = 5;
 
 // variables for rock paper scissors choices
-var rockBtn = document.getElementById('rock');
-var paperBtn = document.getElementById('paper');
-var scissorsBtn = document.getElementById('scissors');
 var allBtn = document.querySelectorAll('.btn');
+// variables for text output of page title, choices, scores, and winner.
+var title = document.getElementById('title')
+var roundNumber = document.getElementById('round-number');
+var youChose = document.getElementById('you-chose');
+var computerChose = document.getElementById('computer-chose');
+var humanScoreDisplay = document.getElementById('human-score-display');
+var compScoreDisplay = document.getElementById('comp-score-display');
+var roundWinner = document.getElementById('round-winner');
+var winnerDisplay = document.getElementById('winner-display');
+var refresh = document.getElementById('refresh');
+// variable for all divs in one div (to style/align game)
+var gameContent = document.getElementById('game-content');
+var divs = document.querySelectorAll('div');
+var text = document.querySelectorAll('.text');
+var gameBody = document.getElementById('game-body');
+var choicesDiv = document.getElementById('choices');
+var scoresDiv = document.getElementById('scores');
+var directions = document.querySelector('p');
+
+
 
 allBtn.forEach((button) => {
     button.addEventListener("click", playGame);
 });
 
 function playGame(e) {
-    if (currentRound > roundLimit) {
-        console.log("Game over! Please refresh to play again.");
-    }
-
-    console.log("ROUND: " + currentRound);
+    roundNumber.textContent = "ROUND: " + currentRound;
     humanChoice = e.target.title;
-    console.log("You chose: " + humanChoice);
+    youChose.textContent = "You chose: " + humanChoice;
 
     getComputerChoice();
     playRound(humanChoice, computerChoice);
 
-    console.log("Your score: " + humanScore);
-    console.log("Bot score: " + computerScore);
+    humanScoreDisplay.textContent = "Your score: " + humanScore;
+    compScoreDisplay.textContent = "Bot score: " + computerScore;
 
     currentRound ++;
     
     if (currentRound > roundLimit) {
         if (humanScore > computerScore) {
+            winnerDisplay.textContent = "GAME OVER: YOU WON!";
             console.log("GAME OVER: YOU WON!");
         } else if (computerScore > humanScore) {
+            winnerDisplay.textContent = "GAME OVER: YOU LOST!"; 
             console.log("GAME OVER: YOU LOST!");
         } else {
+            winnerDisplay.textContent = "GAME OVER: TIED MATCH!"
             console.log("GAME OVER: TIED MATCH!");
+        }
+        refresh.textContent = "Please refresh to play again."
+
+        if (winnerDisplay.textContent.includes("TIED")) {
+            winnerDisplay.style.color = "#4c77ed";
+        } else if (winnerDisplay.textContent.includes("LOST")) {
+            winnerDisplay.style.color = "#ed4c4c";
+        } else if (winnerDisplay.textContent.includes("WON")) {
+            winnerDisplay.style.color = "#f1f2c3";
         }
     }
 
@@ -56,7 +89,7 @@ function getComputerChoice() {
     } else {
         computerChoice = "scissors";
     }
-    console.log("Computer chose: " + computerChoice);
+    computerChose.textContent = "Computer chose: " + computerChoice;
     return computerChoice;
 }
 
@@ -64,20 +97,94 @@ function getComputerChoice() {
 // Compare choices and declare if human won or lost. Increment score
 function playRound(humanChoice, computerChoice) {            
     if (computerChoice === humanChoice) {
-        console.log("Tie! you both chose " + humanChoice + "!");
+        roundWinner.textContent = "Tie! you both chose " + humanChoice + "!";
     } else if (computerChoice === "rock" && humanChoice ==="scissors") {
         computerScore ++;
-        console.log("You lost this round! rock beats scissors.");
+        roundWinner.textContent = "You lost this round! rock beats scissors.";
     } else if (computerChoice === "paper" && humanChoice ==="rock") {
         computerScore ++;
-        console.log("You lost this round! paper beats rock.");
+        roundWinner.textContent = "You lost this round! paper beats rock.";
     } else if (computerChoice === "scissors" && humanChoice ==="paper") {
         computerScore ++;
-        console.log("You lost this round! scissors beats paper.");
+        roundWinner.textContent = "You lost this round! scissors beats paper."
     } else {
         humanScore ++;
-        console.log("You won this round! " + humanChoice  + " beats " + computerChoice + "!");
+        roundWinner.textContent = "You won this round! " + humanChoice  + " beats " + computerChoice + "!";
+    }
+
+    if (roundWinner.textContent === "Tie! you both chose " + humanChoice + "!") {
+        roundWinner.style.color = "#4c77ed";
+    } else if (roundWinner.textContent.includes("You lost")) {
+        roundWinner.style.color = "#ed4c4c";
+    } else if (roundWinner.textContent.includes("You won")) {
+        roundWinner.style.color = "#f1f2c3";
     }
    
 }
     
+
+// styling 
+document.body.style.display = "flex";
+document.body.style.justifyContent = "center";
+//document.body.style.alignItems = "center";  // Centers vertically
+document.body.style.height = "60vh";
+document.body.style.backgroundColor = "#1f1f1f";
+
+gameContent.style.display = "flex";
+gameContent.style.flexDirection = "column";
+gameContent.style.alignItems = "center";
+gameBody.style.display = "flex";
+gameBody.style.flexDirection = "column";
+gameBody.style.alignItems = "center";
+gameBody.style.justifyContent = "center";
+gameBody.style.marginTop = "28px";
+
+text.forEach((one) => {
+    one.style.color = "#89bc59";
+})
+
+divs.forEach((div) => {
+    div.style.padding = "10px";
+})
+
+allBtn.forEach((button) => {
+    button.style.width = "150px";  
+    button.style.height = "50px";  
+    button.style.backgroundColor = "#506f32";  
+    button.style.borderColor = "#333";  
+    button.style.color = "white";  
+    button.style.marginRight = "20px";
+    button.style.fontSize = "18px";
+    button.style.fontFamily = "Verdana";
+});
+
+roundNumber.style.textDecoration = "underline";
+roundNumber.style.fontSize = "25px";
+roundNumber.style.marginTop = "20px";
+
+
+
+choicesDiv.style.display = "flex";
+youChose.style.marginRight = "70px";
+computerChose.style.marginLeft = "70px";
+youChose.style.color = "#edda4c";
+computerChose.style.color = "#eda24c";
+choicesDiv.style.fontSize = "20px";
+
+roundWinner.style.fontSize = "30px";
+
+scoresDiv.style.display = "flex";
+humanScoreDisplay.style.marginRight = "10px";
+compScoreDisplay.style.marginLeft = "10px";
+humanScoreDisplay.style.color = "#898a7f";
+compScoreDisplay.style.color = "#898a7f";
+
+winnerDisplay.style.fontSize = "40px";
+
+title.style.fontSize = "40px";
+title.style.fontFamily = "Georgia";
+title.style.position = "absolute";
+title.style.marginTop = "40px";
+
+directions.style.marginTop = "130px";
+directions.style.color = "white";
